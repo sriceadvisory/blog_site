6d87cb2427fcb7c74097fd10234ad28098e93763
srice2024 <srice@legacyforcemedia.com>
2024-12-24 23:39:20 -0800

    Edit - Created classes for posting and authors for blog posts

--- blog_site/blog_site/srice_site/blog_site/models.py
+++ blog_site/blog_site/srice_site/blog_site/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=40)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=150)
+    image_name = models.CharField(max_length=150)
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, related_name='blog_posts', on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, related_name='posts')
